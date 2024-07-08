10de9e744ed79ce112e2b88a6f035d5e34cf4706
Alexsandr Ryzhkov <alexsandr.ryzhkov04@gmail.com>
2024-07-09 02:47:40 +0300

    feat(#no_task): Fix comment

--- test/setup.ts
+++ test/setup.ts
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/vitest';
 
 // Библиотека PrimeReact имеет баг
 // во время тестирования выдает ошибку компиляции css
-// эта ошибка не влияет на тестирование но мешает чтению логов тестов
+// эта ошибка не влияет на тестирование но мешает чтению логов
 // поэтому я отключил ее командой ниже
 const originalConsoleError = console.error;
 const jsDomCssError = "Error: Could not parse CSS stylesheet";
